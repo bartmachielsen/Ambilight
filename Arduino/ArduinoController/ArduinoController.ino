a9d5eb04f336b8efee199d90a9566d96bd18b2d0
Bart Machielsen <bartmachielsen@gmail.com>
2016-05-02 14:38:07 +0200

    Arduino File containing the source code needed for the ledstripcontroller. SerialLibrary RXTXComm addition

---
+++ Arduino/ArduinoController/ArduinoController.ino
@@ -0,0 +1,34 @@
+#include <Adafruit_NeoPixel.h>
+
+#define PIN            6
+#define NUMPIXELS     74
+
+ unsigned int values[4];
+ int i = 0;
+
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+
+void setup() {
+  Serial.begin(9600);
+  pixels.begin();
+  for(int z = 0; z < NUMPIXELS; z++){
+    pixels.setPixelColor(z,0,0,0);
+  }
+ 
+    pixels.show();
+}
+
+void loop() {
+  if (i == 4) {
+        pixels.setPixelColor(values[0],(values[1]*2.0),(values[2]*2.0),(values[3]*2.0));
+        pixels.show();  
+        i = 0;   
+ }else{
+  if(Serial.available() > 0){
+      unsigned int waarde = Serial.read();
+      values[i] = waarde;
+      i++;
+  }
+ }
+ 
+}
